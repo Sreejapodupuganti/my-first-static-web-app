133a9dbd7756f0f513723db0a824441f646cc912
Sreejapodupuganti <114600930+Sreejapodupuganti@users.noreply.github.com>
2025-04-03 22:11:49 +0000

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to My First Static Web Site</h1>
-     <h5>My Name is Sreeja Podupuganti</h5>
+     <h1>My Name is Sreeja Podupuganti</h1>
+     <h4>Welcome to My First Static Web Site</h4>
     </div>
   );
 }
